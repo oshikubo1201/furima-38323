10832e9c008a1e4d206f6e3d6d31cf2f07acf33c
oshikubo1201 <s.oshikubo1201@gmail.com>
2022-08-10 15:25:13 +0900

    単体テストコードの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,8 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-
+  
+  validates :image     , presence: true
   validates :items_name, presence: true 
   validates :explanation, presence: true
   validates :category_id, presence: true 
@@ -16,7 +17,8 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true 
   validates :prefecture_id, presence: true
   validates :days_to_ship_id, presence: true 
-  validates :price, presence: true
+  validates :price, presence: true,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to:9999999 },format: { with: /\A[0-9]+\z/ }
+
   validates :user, presence: true   
 
   validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"}
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,19 @@
 FactoryBot.define do
   factory :item do
-    items_name
-    explanation
-    category_id
-    situation_id
-    delivery_charge_id
-    prefecture_id
-    days_to_ship_id
-    price
-    user
+    image               {}
+    items_name          {'test'}
+    explanation         {'A1aあaaaaaiepfoifjleiafhoi;athw-fjpi!efjop'}
+    category_id         {2}
+    situation_id        {2}
+    delivery_charge_id  {2}
+    prefecture_id       {2}
+    days_to_ship_id     {2}
+    price               {99999}
+    
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
