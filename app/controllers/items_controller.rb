d11099c1483a2e604d4875ef2c23caf8d3b7b554
oshikubo1201 <s.oshikubo1201@gmail.com>
2022-08-11 22:42:34 +0900

    update機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -23,10 +23,19 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+
   end
 
   def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else render :edit
+    end
+
   end
 
   private
